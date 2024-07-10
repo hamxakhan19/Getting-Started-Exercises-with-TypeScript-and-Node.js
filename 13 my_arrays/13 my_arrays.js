ecc5c3078d4c4f44da4864326934f07e4fdf0cfb
Hamza khan <160407087+hamxakhan19@users.noreply.github.com>
2024-07-11 01:12:05 +0500

    Add files via upload

---
+++ 13 my_arrays/13 my_arrays.js
@@ -0,0 +1,6 @@
+"use strict";
+let transportation = ['Dodge Challenger', 'G Wagon', 'Bantely', 'Toyota Supra Mk4', 'La Ferrari', 'Bugatti Chiron'];
+for (let i = 0; i < transportation.length; i++) {
+    console.log('I would like to own a ' + transportation[i]);
+}
+;
---
+++ 13 my_arrays/13 my_arrays.ts
@@ -0,0 +1,7 @@
+let transportation :string[] = ['Dodge Challenger','G Wagon','Bantely','Toyota Supra Mk4','La Ferrari','Bugatti Chiron'] ;
+
+for(let i=0; i<transportation.length; i++){
+
+    console.log('I would like to own a ' + transportation[i])
+
+};
