8d83f13ce1e5b4fb30d7174c42dff811382573ee
Hamza khan <160407087+hamxakhan19@users.noreply.github.com>
2024-07-11 01:13:42 +0500

    Add files via upload

---
+++ 14 guest_list/14 guest_list.ts
@@ -0,0 +1,7 @@
+let guest_list :string[] = ['moiz','huzaifa','bilal','alyan','amir','hamza rafi','ali','ayan','muzammil'];
+
+for(let i=0; i<guest_list.length; i++){
+
+    console.log('Dear Mr.'+ guest_list[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nTHANK YOU :)\n\n')
+    
+}
