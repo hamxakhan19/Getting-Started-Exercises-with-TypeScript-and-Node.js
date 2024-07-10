019df966fa2e5ad79d85a54ca84a6aa137aeb626
Hamza khan <160407087+hamxakhan19@users.noreply.github.com>
2024-07-11 00:56:08 +0500

    Add files via upload

---
+++ 6 Stripping_Names/6 Stripping_Names.js
@@ -0,0 +1,5 @@
+"use strict";
+const Name = '   \n\tImran Khan\t\n   ';
+console.log(Name);
+const without_whitespace = Name.trim();
+console.log(without_whitespace);
---
+++ 6 Stripping_Names/6 Stripping_Names.ts
@@ -0,0 +1,7 @@
+const Name : string = '   \n\tImran Khan\t\n   ';
+
+console.log(Name);
+
+const without_whitespace :string = Name.trim();
+
+console.log(without_whitespace);
