4abe73df7b6e94400ec2a53c3e57033d848b4199
Hamza khan <160407087+hamxakhan19@users.noreply.github.com>
2024-07-11 01:32:19 +0500

    Add files via upload

---
+++ 18 seeing_the_world/18-seeing_the_world.ts
@@ -0,0 +1,33 @@
+
+//store the locations in a array. Array is not in alphabetical order.
+let places :string[] =['egypt','denmark','canada','belgium','America',]; 
+
+// print array in its orignal order.
+console.log('orignal '+ places);
+
+// print array in alphabetic order without modifiying the actual list.
+console.log('copy ' + [...places].sort());
+
+//showing that array is still its orignal order.
+console.log('orignal '+ places);
+
+//print array in reverse alphabetical order without changing the order of orignal list.
+console.log('copy ' + [...places].sort().reverse());
+
+//showing that array is still in its orignal.
+console.log('orignal '+ places);
+
+// reversing the order of list and printing the array to show that its orignal order has changed.
+console.log('orignal '+ places.reverse());
+
+//again reversing the order of list and printing the list to show its back to its orignal order.
+console.log('orignal '+ places.reverse());
+
+//sort array so it is stored in alphabetical order and printing the array to show that its order has been changed.
+console.log('orignal '+ places.sort());
+
+//sort to change array so it is stored in reversed alphabetical order and printing the list to show that its order has changed.
+console.log('orignal '+ places.sort().reverse());
+
+
+
