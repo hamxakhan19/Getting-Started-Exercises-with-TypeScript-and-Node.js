d6f418da7fbc427d4929c963c0afe538eab1a0cf
Hamza khan <160407087+hamxakhan19@users.noreply.github.com>
2024-07-11 01:09:46 +0500

    Add files via upload

---
+++ 12  greetings/12  greetings.js
@@ -0,0 +1,6 @@
+"use strict";
+let names = ['hamza', 'alyan', 'bilal', 'moiz', 'huzaifa', 'faiz', 'abdullah', 'amir', 'adeeb', 'furqan', 'subhan', 'zubair', 'wasay', 'ali raza', 'zaidan', 'ali'];
+const message = ' is my friend. ';
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i] + message);
+}
---
+++ 12  greetings/12  greetings.ts
@@ -0,0 +1,8 @@
+let names:string []= ['hamza','alyan','bilal','moiz','huzaifa','faiz','abdullah','amir','adeeb','furqan','subhan','zubair','wasay','ali raza','zaidan','ali'];
+
+const message : string = ' is my friend. ';
+
+for(let i=0; i<names.length; i++){
+
+    console.log(names[i]+message) ;
+}
